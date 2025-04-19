a8745fc59d3d40f965b3f6688754dd23d74ba4e6
Adam Brown <adamwbrown@gmail.com>
2025-04-18 21:58:29 -0700

    Add measureAndReport

---
+++ app/src/main/kotlin/com/darkrockstudios/app/securecamera/TimeUtils.kt
@@ -0,0 +1,27 @@
+package com.darkrockstudios.app.securecamera
+
+/**
+ * Measures the execution time of the provided block of code in milliseconds.
+ * @param block The code block to measure.
+ * @return A Pair containing the result of the block execution and the time taken in milliseconds.
+ */
+inline fun <T> measureTime(block: () -> T): Pair<T, Long> {
+	val startTime = System.currentTimeMillis()
+	val result = block()
+	val endTime = System.currentTimeMillis()
+	val executionTime = endTime - startTime
+	return Pair(result, executionTime)
+}
+
+/**
+ * Measures the execution time of the provided block of code, prints the time with a message,
+ * and returns the result of the block execution.
+ * @param message The message to print along with the measured time.
+ * @param block The code block to measure.
+ * @return The result of the block execution.
+ */
+inline fun <T> measureAndReport(message: String, block: () -> T): T {
+	val (result, time) = measureTime(block)
+	println("$message: $time ms")
+	return result
+}
