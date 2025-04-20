7385de7ba03c1981f9fc0caacbb4533d77fd171e
Adam Brown <adamwbrown@gmail.com>
2025-04-19 18:43:57 -0700

    Prevent screenshots & recents

--- app/src/main/kotlin/com/darkrockstudios/app/securecamera/MainActivity.kt
+++ app/src/main/kotlin/com/darkrockstudios/app/securecamera/MainActivity.kt
@@ -2,6 +2,7 @@ package com.darkrockstudios.app.securecamera
 
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,12 @@ class MainActivity : ComponentActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		window.setFlags(
+			WindowManager.LayoutParams.FLAG_SECURE,
+			WindowManager.LayoutParams.FLAG_SECURE
+		)
+
 		enableEdgeToEdge()
 		setContent {
 			App(capturePhoto)
