2111b46f3342c2a5c44545964446c5fce55b084a
Adam Brown <adamwbrown@gmail.com>
2025-05-03 00:27:28 -0700

    Pad the spinner

--- app/src/main/kotlin/com/darkrockstudios/app/securecamera/obfuscation/ObfuscatePhotoTopBar.kt
+++ app/src/main/kotlin/com/darkrockstudios/app/securecamera/obfuscation/ObfuscatePhotoTopBar.kt
@@ -5,14 +5,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddBox
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -54,7 +47,9 @@ fun ObfuscatePhotoTopBar(
 			if (!isCreatingRegion) {
 				if (isFindingFaces) {
 					CircularProgressIndicator(
-						modifier = Modifier.size(24.dp),
+						modifier = Modifier
+							.padding(8.dp)
+							.size(24.dp),
 						color = MaterialTheme.colorScheme.onPrimaryContainer,
 						strokeWidth = 2.dp
 					)
